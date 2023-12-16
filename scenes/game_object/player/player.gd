3ae5e434dbeeef4eb21d08e90e785534254a0dba
Steven Tuckness <stuckne1@gmail.com>
2023-12-16 12:52:59 -0600

    feat(player): add WSDL controls as well

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -25,8 +25,8 @@ func _process(delta):
 		visuals.scale = Vector2(move_sign, 1)
 
 func get_movement_vector() -> Vector2:	
-	var x_movement = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	var y_movement = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 
 	return Vector2(x_movement, y_movement)
 	
